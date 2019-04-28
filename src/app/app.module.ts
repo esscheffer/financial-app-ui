5cb050f7d39e02793210f76589ae090476731e01
Erik de Souza Scheffer <erikscheffer@yahoo.com>
2019-04-28 14:26:49 -0300

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {ButtonModule, InputTextModule} from 'primeng/primeng';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    InputTextModule,
+    ButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
