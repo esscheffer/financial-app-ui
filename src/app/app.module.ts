471bd6fd497ecdc00b8cac9532119d5f96b0a875
Erik de Souza Scheffer <erikscheffer@yahoo.com>
2019-06-04 22:58:15 -0300

    Added table buttons tooltips.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {ButtonModule, InputTextModule} from 'primeng/primeng';
+import {ButtonModule, InputTextModule, TooltipModule} from 'primeng/primeng';
 import {TableModule} from 'primeng/table';
 
 @NgModule({
@@ -13,7 +13,8 @@ import {TableModule} from 'primeng/table';
     BrowserModule,
     InputTextModule,
     ButtonModule,
-    TableModule
+    TableModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
