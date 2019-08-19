f53a6207672564f301b328e381886b196a66ab1d
Erik de Souza Scheffer <erikscheffer@yahoo.com>
2019-08-19 12:40:12 -0300

    Financial entry register layout
    Creation of the component for the financial entry register
    Implemented base layout for the component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,28 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {ButtonModule, InputTextModule, TooltipModule} from 'primeng/primeng';
+import {ButtonModule, InputTextareaModule, InputTextModule, TooltipModule} from 'primeng/primeng';
 import {TableModule} from 'primeng/table';
 import {FinancialEntrySearchComponent} from './financial-entry-search/financial-entry-search.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {PersonSearchComponent} from './person-search/person-search.component';
+import {FinancialEntryRegisterComponent} from './financial-entry-register/financial-entry-register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FinancialEntrySearchComponent,
     NavbarComponent,
-    PersonSearchComponent
+    PersonSearchComponent,
+    FinancialEntryRegisterComponent
   ],
   imports: [
     BrowserModule,
     InputTextModule,
     ButtonModule,
     TableModule,
-    TooltipModule
+    TooltipModule,
+    InputTextareaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/financial-entry-register/financial-entry-register.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-financial-entry-register',
+  templateUrl: './financial-entry-register.component.html',
+  styleUrls: ['./financial-entry-register.component.css']
+})
+export class FinancialEntryRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
